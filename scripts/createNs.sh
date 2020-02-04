a7d3c3b77174be6a202bbcae60df05d9d96c8347
Sherba Mikhail <sherba@elewise.com>
2020-02-04 14:43:52 +0400

    ci upd

---
+++ scripts/createNs.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+TAG=$1
+
+if [ "$TAG" == "develop"]; then echo 'export NAMESPACE=develop' >> $BASH_ENV; elif [[ "$TAG" =! prodbuild* ]]; then echo 'export NAMESPACE=$TAG' >> $BASH_ENV; else echo 'export NAMESPACE=production' >> $BASH_ENV; fi
+kubectl config use-context kube-east
+kubectl get ns $NAMESPACE --insecure-skip-tls-verify=true
+if [ $? -ne 0]; then kubectl create ns $NAMESPACE --insecure-skip-tls-verify=true && kubectl get secret -n databases mysql -o yaml --export --insecure-skip-tls-verify=true | kubectl apply -n $NAMESPACE --insecure-skip-tls-verify=true -f - && cat <<EOF | kubectl appply -n $NAMESPACE -f -
+---
+apiVersion: v1
+items:
+- apiVersion: v1
+  kind: Service
+  metadata:
+    name: mongodb
+  spec:
+    externalName: mongodb.databases.svc.cluster.local
+    ports:
+    - name: mongodb
+      port: 27017
+      protocol: TCP
+      targetPort: mongodb
+    sessionAffinity: None
+    type: ExternalName
+---
+- apiVersion: v1
+  kind: Service
+  metadata:
+    name: mysql
+  spec:
+    externalName: mysql.databases.svc.cluster.local
+    ports:
+    - name: mysql
+      port: 3306
+      protocol: TCP
+      targetPort: mysql
+    sessionAffinity: None
+    type: ExternalName
+---
+- apiVersion: v1
+  kind: Service
+  metadata:
+    name: redis-master
+  spec:
+    externalName: redis-master.databases.svc.cluster.local
+    ports:
+    - name: redis
+      port: 6379
+      protocol: TCP
+      targetPort: redis
+    sessionAffinity: None
+    type: ExternalName
+EOF
+
