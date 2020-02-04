b84678b3a6bf5217d5c60d13b35c15e6eb45bd4b
Sherba Mikhail <sherba@elewise.com>
2020-02-04 15:53:44 +0400

    ci typo

--- scripts/createNs.sh
+++ scripts/createNs.sh
@@ -4,7 +4,6 @@ NAMESPACE=$1
 
 kubectl get ns $NAMESPACE --insecure-skip-tls-verify=true
 if [ $? -ne 0]; then kubectl create ns $NAMESPACE --insecure-skip-tls-verify=true && kubectl get secret -n databases mysql -o yaml --export --insecure-skip-tls-verify=true | kubectl apply -n $NAMESPACE --insecure-skip-tls-verify=true -f - && cat <<EOF | kubectl appply -n $NAMESPACE -f -
----
 apiVersion: v1
 items:
 - apiVersion: v1
